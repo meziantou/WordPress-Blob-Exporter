65f477d9af43d8c703e67b7af29239206004e488
meziantou <meziantou@live.fr>
2014-08-05 22:35:02 +0200

    Version 1.0

---
+++ WordPressBlobExporter/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WordPressBlobExporter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSourcePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxDestinationPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxSourcePath
+            // 
+            this.textBoxSourcePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSourcePath.Location = new System.Drawing.Point(102, 12);
+            this.textBoxSourcePath.Name = "textBoxSourcePath";
+            this.textBoxSourcePath.Size = new System.Drawing.Size(297, 20);
+            this.textBoxSourcePath.TabIndex = 0;
+            this.textBoxSourcePath.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "XML file:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(405, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonSrcPath_Click);
+            // 
+            // textBoxDestinationPath
+            // 
+            this.textBoxDestinationPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDestinationPath.Location = new System.Drawing.Point(102, 41);
+            this.textBoxDestinationPath.Name = "textBoxDestinationPath";
+            this.textBoxDestinationPath.Size = new System.Drawing.Size(297, 20);
+            this.textBoxDestinationPath.TabIndex = 0;
+            this.textBoxDestinationPath.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Target directory:";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(405, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ButtonDstPath_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExport.Enabled = false;
+            this.buttonExport.Location = new System.Drawing.Point(405, 68);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 23);
+            this.buttonExport.TabIndex = 3;
+            this.buttonExport.Text = "&Export";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 68);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 103);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxDestinationPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSourcePath);
+            this.Name = "Form1";
+            this.Text = "WordPress Blob Exporter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSourcePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxDestinationPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WordPressBlobExporter/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WordPressBlobExporter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSrcPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "XML file (*.xml)|*.xml";
+            dialog.CheckFileExists = true;
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Yes)
+            {
+                textBoxSourcePath.Text = dialog.FileName;
+            }
+        }
+
+        private void ButtonDstPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = true;
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult == DialogResult.OK || dialogResult == DialogResult.Yes)
+            {
+                textBoxDestinationPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            buttonExport.Enabled = !string.IsNullOrWhiteSpace(textBoxSourcePath.Text) && !string.IsNullOrWhiteSpace(textBoxDestinationPath.Text);
+        }
+
+        private async void buttonExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                buttonExport.Enabled = false;
+                progressBar1.Visible = true;
+                progressBar1.Value = 0;
+
+                var dstPath = textBoxDestinationPath.Text;
+                if (!Directory.Exists(dstPath))
+                {
+                    Directory.CreateDirectory(dstPath);
+                }
+
+                ConcurrentBag<string> errors = new ConcurrentBag<string>();
+
+                var document = new XmlDocument();
+                document.Load(textBoxSourcePath.Text);
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(document.NameTable);
+                nsmgr.AddNamespace("wp", "http://wordpress.org/export/1.2/");
+                var nodes = document.SelectNodes("//item[wp:attachment_url]", nsmgr);
+                if (nodes != null)
+                {
+                    progressBar1.Maximum = nodes.Count;
+
+                    await Task.Run(() =>
+                    {
+                        Parallel.ForEach(nodes.OfType<XmlElement>(), node =>
+                        {
+                            var nodeId = node.SelectSingleNode("wp:post_id", nsmgr);
+                            if (nodeId == null)
+                                return;
+
+                            var nodeUrl = node.SelectSingleNode("wp:attachment_url", nsmgr);
+                            if (nodeUrl == null)
+                                return;
+
+                            try
+                            {
+                                var id = nodeId.InnerText;
+                                string extension = null;
+                                try
+                                {
+                                    extension = Path.GetExtension(nodeUrl.InnerText);
+                                }
+                                catch
+                                { }
+                                var path = Path.Combine(dstPath, id + extension);
+                                using (WebClient client = new WebClient())
+                                {
+                                    client.DownloadFile(nodeUrl.InnerText, path);
+                                }
+                            }
+                            catch
+                            {
+                                errors.Add(nodeUrl.InnerText);
+                            }
+
+                            progressBar1.Invoke(new Action(() => progressBar1.Increment(1)));
+                        });
+                    });
+
+                    if (errors.Count > 0)
+                    {
+                        MessageBox.Show("Errors:" + Environment.NewLine + string.Join(Environment.NewLine, errors), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                buttonExport.Enabled = true;
+                progressBar1.Visible = false;
+            }
+        }
+    }
+}
